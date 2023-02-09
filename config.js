1a64a6d8f4a235b6f553f8bb5146730500d20eff
Peaces-a <122713081+Peaces-a@users.noreply.github.com>
2023-02-09 11:17:47 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,37 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Peace surveillance',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793676888-5fb37cbe869a8130bdb629bf',/*Peace-blog */
+    'm793676893-f91c45ad291326f8087841ba',/*Peace-GR */
+    'm793676894-705353a412a9daf528710e75',/*Peace-twikoo */
+    'm793676885-b133ec6f0dcfa21d2fe8e224',/*Peace-9fl.net */
+    'm793676897-1b69639f85d8412a8301734d',/*Peace-qexo */
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://9fl.net/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/Peacea-a/'
+    },
+    {
+      text: 'Blog',
+      url: 'https://www.tiaq.cn/'
+    },
+  ],
+};
